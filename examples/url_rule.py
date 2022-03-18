a460ff44d8817dbcf612814d77b872084154c94d
amirnasri <amnasri@gmail.com>
2022-03-17 19:42:24 -0700

    Added basic example.
    The example shows how to create an app and
    add a url rule.

---
+++ examples/url_rule.py
@@ -0,0 +1,19 @@
+from easy_web import EasyWeb
+
+app = EasyWeb(__name__)
+
+def test_url(request):
+    return "hello world!"
+
+app.add_url_rule("/test", "test_url", test_url)
+
+
+def test_url_with_param(request, **values):
+    return str(values)
+
+app.add_url_rule("/test/<int:id>", "test_url_with_param", test_url_with_param)
+
+
+app.run()
+
+
